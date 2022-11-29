7aecfc1ff64638905234d28358813a205228cd23
Rafael Ferreira <rafael.bros.ferreira@gmail.com>
2022-11-29 14:24:56 -0300

    SellerList

---
+++ src/model/services/SellerService.java
@@ -0,0 +1,52 @@
+package model.services;
+
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerService {
+	
+	private SellerDao dao = DaoFactory.createSellerDao();
+	
+	public List<Seller> findAll(){
+		return dao.findAll();
+		
+	}
+	
+		
+	public void saveOrUpdate(Seller obj) {
+		
+		if(obj.getId()==null) {
+			dao.insert(obj);
+			
+		}
+		else {
+			
+			dao.update(obj);
+		}
+		
+		
+	}
+	
+	public void remove(Seller obj) {
+		
+		dao.deleteById(obj.getId());
+		
+	}
+		
+		
+		/*Mocked
+		List<Seller> list = new ArrayList<>();
+		list.add(new Seller(1, "Books"));
+		list.add(new Seller(2, "Computers"));
+		list.add(new Seller(3, "Electronics"));
+		
+		return list;
+		*/
+		
+	
+
+}
+
